73d75a262723aab45956b7baa99301d0e21e29d9
Jesús Espino <jespinog@gmail.com>
2015-07-13 16:32:30 +0200

    Add initial rules version

--- src/uxbox/workspace/canvas/views.cljs
+++ src/uxbox/workspace/canvas/views.cljs
@@ -1,4 +1,53 @@
-(ns uxbox.workspace.canvas.views)
+(ns uxbox.workspace.canvas.views
+  (:require [cuerdas.core :as str]))
+
+(defn vertical-rule
+  [height start-height zoom]
+  (let [padding 20
+        big-ticks-mod 100
+        mid-ticks-mod 50
+        step-size 10
+        ticks (range (- padding start-height) (- height start-height padding) step-size)
+
+        lines (fn
+          [position value padding]
+          (cond
+             (= (mod value big-ticks-mod) 0)
+                [:g
+                 [:line {:y1 position :y2 position :x1 5 :x2 padding :stroke "black"}]
+                 [:text {:y position :x 5 :transform (str/format "rotate(90 0 %s)" position)} value]]
+             (= (mod value mid-ticks-mod) 0)
+                [:line {:y1 position :y2 position :x1 10 :x2 padding :stroke "black"}]
+             :else
+                [:line {:y1 position :y2 position :x1 15 :x2 padding :stroke "black"}]))
+        ]
+    [:g.vertical-rule
+     [:rect {:x 0 :y padding :height height :width padding :fill "gray"}]
+     (map #(lines (+ %1 start-height) %1 padding) ticks)]))
+
+(defn horizontal-rule
+  [width start-width zoom]
+  (let [padding 20
+        big-ticks-mod 100
+        mid-ticks-mod 50
+        step-size 10
+        ticks (range (- padding start-width) (- width start-width padding) step-size)
+        lines (fn
+          [position value padding]
+          (cond
+             (= (mod value big-ticks-mod) 0)
+                [:g
+                 [:line {:x1 position :x2 position :y1 5 :y2 padding :stroke "black"}]
+                 [:text {:x (+ position 2) :y 13} value]]
+             (= (mod value mid-ticks-mod) 0)
+                [:line {:x1 position :x2 position :y1 10 :y2 padding :stroke "black"}]
+             :else
+                [:line {:x1 position :x2 position :y1 15 :y2 padding :stroke "black"}]))
+        ]
+    [:g.horizontal-rule
+     [:rect {:x padding :y 0 :width width :height padding :fill "gray"}]
+     [:rect {:x 0 :y 0 :width padding :height padding :fill "gray"}]
+     (map #(lines (+ %1 start-width) %1 padding) ticks)]))
 
 ;; Transform from the "shape" internal datastructure to SVG tags
 (defmulti shape->svg :shape)
@@ -15,6 +64,9 @@
 
         page-width (get-in @db [:page :width])
         page-height (get-in @db [:page :height])
+
+        document-start-x (- 500 (/ page-width 2))
+        document-start-y (- 750 (/ page-height 2))
 
         ;; Get a group of ids and retrieves the list of shapes
         ids->shapes (fn [shape-ids]
@@ -38,6 +90,8 @@
                         (map group-svg))]
 
     [:svg {:width viewport-height :height viewport-width}
+     [horizontal-rule viewport-width document-start-x 100]
+     [vertical-rule viewport-height document-start-y 100]
      [:svg  {:x 50 :y 50 :width page-width :height page-height};; Document
       [:rect {:x 0 :y 0 :width "100%" :height "100%" :fill "white"}]
       (apply vector :svg shapes-svg)]
