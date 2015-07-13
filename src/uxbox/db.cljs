c8f6998b6599dd463a233bb2617337eeda62b572
alonso.torres <alonso.torres@kaleidos.net>
2015-07-13 14:21:03 +0200

    Creates state for the rendering of pages and sample rendering of rectangles and lines

--- src/uxbox/db.cljs
+++ src/uxbox/db.cljs
@@ -57,6 +57,31 @@
                        :last-update (js/Date. 2010 10 1)
                        :created (js/Date. 2010 9 1)
                        :page-count 2
-                       :comment-count 4}]})
+                       :comment-count 4}]
+
+                    ;; Rectangles: http://www.w3.org/TR/SVG/shapes.html#RectElement
+                    ;; Lines: http://www.w3.org/TR/SVG/shapes.html#LineElement
+                    ;; Style properties http://www.w3.org/TR/SVG/propidx.html
+
+                    :page
+
+                    {:title "My awesome page"
+                     :author "Bobby Tables"
+
+                     :width 640
+                     :height 1080
+
+                     :shapes {"id1" {:shape :rectangle
+                                     :x 0 :y 0 :width 200 :height 200 :fill "#cacaca" :stroke "black"} ;; Rectangle
+                              "id2" {:shape :rectangle
+                                     :x 20 :y 20 :width 160 :height 160 :rx 5 :ry 5 :fill "white" :stroke "#cacaca"} ;; Rounded rectangle
+                              "id3" {:shape :line
+                                     :x1 20 :y1 20 :x2 180 :y2 180 :color "blue" :width 4}
+                              "id4" {:shape :line
+                                     :x1 180 :y1 20 :x2 20 :y2 180 :color "blue" :width 4}}
+
+                     :groups {"gid1" {:name "Box 1" :order 1 :visible true  :locked false :icon :square :shapes ["id1"]}
+                              "gid2" {:name "Box 2" :order 2 :visible true  :locked false :icon :circle :shapes ["id2"]}
+                              "gid3" {:name "Cross" :order 3 :visible true  :locked false :icon :line   :shapes ["id3" "id4"]}}}})
 
 (defonce app-state (atom initial-state))
